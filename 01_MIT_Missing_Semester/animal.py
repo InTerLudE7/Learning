40f49ebd21aa6dd4085860660244a6b6ac52087d
Xin Yang <xyang1260@gmail.com>
2025-05-14 16:39:53 -0400

    add wolf functionality

--- 01_MIT_Missing_Semester/animal.py
+++ 01_MIT_Missing_Semester/animal.py
@@ -3,9 +3,14 @@ import sys
 def dog():
     print('woof!')
 
+def wolf():
+    print('wolf!')
+
 def default():
     if sys.argv[1] == 'dog':
         dog()
+    elif sys.argv[1] == 'wolf':
+        wolf()
     else:
         print('Hello')
 
